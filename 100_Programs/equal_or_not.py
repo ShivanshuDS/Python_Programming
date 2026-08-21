4a16b9ce24e92daeb4f969ab29812c3ab0767179
ShivanshuDS <shivanshu26022007@gmail.com>
2026-08-21 19:41:13 +0530

    100_program

---
+++ 100_Programs/equal_or_not.py
@@ -0,0 +1,8 @@
+# Enter the two numbers and find out that both are equal or not:-
+#  a,b,a==b:-a
+a=int(input('Enter a frist number is:-'))
+b=int(input('Enter a second number is:-'))
+if a==b:
+    print("Both are equal")
+else:
+    print("Both are not equal")
