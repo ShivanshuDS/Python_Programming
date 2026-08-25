90aa4cc9c8c14cb3d5d5b6648f5236031e6406ee
ShivanshuDS <shivanshu26022007@gmail.com>
2026-08-25 21:17:26 +0530

    100Programs

---
+++ 100_Programs/one_is_greater.py
@@ -0,0 +1,7 @@
+# Input the two number and find out which one is greater.
+a=int(input("Enter a first number:-"))
+b=int(input("Enter a second number:-"))
+if a>b:
+    print("a is greater")
+else:
+    print("b is greater")
